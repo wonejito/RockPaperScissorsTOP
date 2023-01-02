23c08ef6d82c3803c1ba1a6c8c3a7454a76b3f1b
wonejito <30444850+wonejito@users.noreply.github.com>
2023-01-02 17:30:17 -0400

    Toy confundio

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
 function getComputerChoice() {
-  let numeroRandom = Math.floor(Math.random() * 3);
-  if (numeroRandom == 0) {
-    return "Rock";
-  } else if (numeroRandom == 1) {
-    return "Scissors";
+  let randomNumber = Math.floor(Math.random() * 3);
+  if (randomNumber == 0) {
+    return "rock";
+  } else if (randomNumber == 1) {
+    return "scissors";
   } else {
-    return "Paper";
+    return "paper";
   }
+}
+
+let paper
+
+// let playerSelection = prompt(/Rock, Scissors or Paper?: /i);
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == 'paper' && computerSelection == 'scissors') {
+    return 'pierdes'
+  } else if (playerSelection == '')
 }

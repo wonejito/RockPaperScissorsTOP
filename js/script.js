a6657120cffdeac7c674f65e8efa95c9cc25452f
wonejito <30444850+wonejito@users.noreply.github.com>
2023-01-05 13:46:45 -0400

    Hice mucho

--- js/script.js
+++ js/script.js
@@ -1,26 +1,39 @@
 function getComputerChoice() {
-  let randomNumber = Math.floor(Math.random() * 3);
-  if (randomNumber == 0) {
-    return "piedra";
-  } else if (randomNumber == 1) {
-    return "papel";
-  } else {
-    return "tijeras";
-  }
+    let randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber == 0) {
+        return "rock";
+    } else if (randomNumber == 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
 }
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection == "piedra" && computerSelection == "papel") {
-    let result = "Pierdes";
-  } else if (playerSelection == "piedra" && computerSelection == "tijeras") {
-    let result = "Ganas";
-  } else if (playerSelection == "papel" && computerSelection == "tijeras") {
-    let result = "Pierdes";
-  } else if (playerSelection == "papel" && computerSelection == "piedra") {
-    let result = "Pierdes";
-  } else if (playerSelection == "tijeras" && computerSelection == "piedra") {
-    let result = "Pierdes";
-  } else if (playerSelection == "tijeras" && computerSelection == "papel") {
-    let result = "Ganas";
-  }
+    let retorno = "Its a tie!";
+    if (playerSelection == "paper" && computerSelection == "rock") {
+        retorno = "Paper beats rock!";
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        retorno = "Paper cant win against scissors";
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        retorno = "Rock cant beat paper";
+    } else if ((playerSelection = "rock" && computerSelection == "scissors")) {
+        retorno = "Rock beats scissors";
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        retorno = "Scissors beats paper";
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        retorno = "Scissors are too weak to win a fight with rock";
+    }
+
+    return retorno;
+}
+
+const playerSelection = getComputerChoice();
+const computerSelection = getComputerChoice();
+//console.log(playRound(playerSelection, computerSelection)); //
+
+function game() {
+    for (i = 0; i < 5; i++) {
+        console.log(playRound(playerSelection, computerSelection));
+    }
 }

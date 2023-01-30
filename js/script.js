2f80e677197576990dd0bbbe627a07c4e5f6210c
wonejito <30444850+wonejito@users.noreply.github.com>
2023-01-30 16:18:07 -0400

    I dont know what im doing

--- js/script.js
+++ js/script.js
@@ -1,45 +1,56 @@
 function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 3);
-    if (randomNumber == 0) {
-        return "rock";
-    } else if (randomNumber == 1) {
-        return "paper";
-    } else {
-        return "scissors";
-    }
+  let randomNumber = Math.floor(Math.random() * 3);
+  if (randomNumber == 0) {
+    return "rock";
+  } else if (randomNumber == 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
 }
 
 function playRound(playerSelection, computerSelection) {
-    let retorno = "Its a tie!";
-    if (playerSelection == "paper" && computerSelection == "rock") {
-        retorno = "Paper beats rock!";
-    } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        retorno = "Paper cant win against scissors";
-    } else if (playerSelection == "rock" && computerSelection == "paper") {
-        retorno = "Rock cant beat paper";
-    } else if ((playerSelection = "rock" && computerSelection == "scissors")) {
-        retorno = "Rock beats scissors";
-    } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        retorno = "Scissors beats paper";
-    } else if (playerSelection == "scissors" && computerSelection == "rock") {
-        retorno = "Scissors are too weak to win a fight with rock";
-    } else if (
-        (playerSelection && computerSelection != "paper") ||
-        "rock" ||
-        "scissos"
-    ) {
-        retorno = "What the fuck is that?";
-    }
+  let retorno = "Its a tie!";
+  if (playerSelection == "paper" && computerSelection == "rock") {
+    retorno = "Paper beats rock!";
+  } else if (playerSelection == "paper" && computerSelection == "scissors") {
+    retorno = "Paper cant win against scissors";
+  } else if (playerSelection == "rock" && computerSelection == "paper") {
+    retorno = "Rock cant beat paper";
+  } else if ((playerSelection = "rock" && computerSelection == "scissors")) {
+    retorno = "Rock beats scissors";
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    retorno = "Scissors beats paper";
+  } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    retorno = "Scissors are too weak to win a fight with rock";
+  } else if (
+    (playerSelection && computerSelection != "paper") ||
+    "rock" ||
+    "scissos"
+  ) {
+    retorno = "What the fuck is that?";
+  }
 
-    return retorno;
+  return retorno;
 }
 
-function game() {
-    for (i = 0; i < 5; i++) {
-        let playerSelection = prompt("Whats your choice?: ").toLowerCase();
-        let computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-    }
-}
+//let buttons = document.querySelectorAll("button");
+let rock = document.querySelector("#rock");
+let scissors = document.querySelector("#scissors");
+let paper = document.querySelector("paper");
 
-game();
+paper.addEventListener("click", playRound);
+rock.addEventListener("click", playRound);
+scissors.addEventListener("click", playRound);
+
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+// function game() {
+//     for (i = 0; i < 5; i++) {
+//         let playerSelection = prompt("Whats your choice?: ").toLowerCase();
+//         let computerSelection = getComputerChoice();
+//         console.log(playRound(playerSelection, computerSelection));
+//     }
+// }
+
+// game();

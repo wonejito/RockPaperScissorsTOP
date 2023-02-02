99274bb1fc216f9050c8fea753947916f9a58024
wonejito <30444850+wonejito@users.noreply.github.com>
2023-02-02 10:30:25 -0400

    Hey yaaa

--- js/script.js
+++ js/script.js
@@ -46,51 +46,73 @@ let scissors = document.querySelector("#scissors");
 let board = document.querySelector("#board");
 let playerpoints = document.querySelector("#playerpoints");
 let computerpoints = document.querySelector("#computerpoints");
+let botones = document.querySelectorAll(".toton");
 
 let computerPoints = 0;
 let playerPoints = 0;
 
-paper.addEventListener("click", function () {
-  computerSelection = getComputerChoice();
-  board.textContent = playRound("paper", computerSelection);
-  if (board.textContent == "paper beats rock") {
-    playerPoints = playerPoints + 1;
-    playerpoints.textContent = playerPoints;
-  } else if (computerSelection == "paper") {
-    playerpoints.textContent;
-    computerpoints.textContent;
-  } else {
-    computerPoints = computerPoints + 1;
-    computerpoints.textContent = computerPoints;
-  }
+botones.forEach((boton) => {
+  boton.addEventListener("click", function () {
+    computerSelection = getComputerChoice();
+    playerSelection = this.textContent;
+    board.textContent = playRound(playerSelection, computerSelection);
+  });
 });
 
-rock.addEventListener("click", function () {
-  computerSelection = getComputerChoice();
-  board.textContent = playRound("rock", computerSelection);
-  if (board.textContent == "rock beats scissors") {
-    playerPoints = playerPoints + 1;
-    playerpoints.textContent = playerPoints;
-  } else if (computerSelection == "rock") {
-    playerpoints.textContent;
-    computerpoints.textContent;
-  } else {
-    computerPoints = computerPoints + 1;
-    computerpoints.textContent = computerPoints;
-  }
-});
+// if (playerPoints == 5) {
+//   playerPoints = 0;
+//   computerPoints = 0;
+//   board.textContent = "Player Wins";
+// } else if (computerPoints == 5) {
+//   playerPoints = 0;
+//   computerPoints = 0;
+//   board.textContent = "Computer Wins";
+// }
 
-scissors.addEventListener("click", function () {
-  computerSelection = getComputerChoice();
-  board.textContent = playRound("scissors", computerSelection);
-  if (board.textContent == "scissors beats paper") {
-    playerPoints = playerPoints + 1;
-    playerpoints.textContent = playerPoints;
-  } else if (computerSelection == "scissors") {
-    playerpoints.textContent;
-    computerpoints.textContent;
-  } else {
-    computerPoints = computerPoints + 1;
-    computerpoints.textContent = computerPoints;
-  }
-});
+// paper.addEventListener("click", function () {
+//   computerSelection = getComputerChoice();
+//   board.textContent = playRound("paper", computerSelection);
+//   if (board.textContent == "paper beats rock") {
+//     playerPoints = playerPoints + 1;
+//     playerpoints.textContent = playerPoints;
+//   } else if (computerSelection == "paper") {
+//     playerpoints.textContent;
+//     computerpoints.textContent;
+//   } else {
+//     computerPoints = computerPoints + 1;
+//     computerpoints.textContent = computerPoints;
+//   }
+// });
+
+// rock.addEventListener("click", function () {
+//   computerSelection = getComputerChoice();
+//   board.textContent = playRound("rock", computerSelection);
+//   if (board.textContent == "rock beats scissors") {
+//     playerPoints = playerPoints + 1;
+//     playerpoints.textContent = playerPoints;
+//   } else if (computerSelection == "rock") {
+//     playerpoints.textContent;
+//     computerpoints.textContent;
+//   } else {
+//     computerPoints = computerPoints + 1;
+//     computerpoints.textContent = computerPoints;
+//   }
+//   if ((computerpoints.textContent = "5")) {
+//     computerpoints.textContent = "";
+//   }
+// });
+
+// scissors.addEventListener("click", function () {
+//   computerSelection = getComputerChoice();
+//   board.textContent = playRound("scissors", computerSelection);
+//   if (board.textContent == "scissors beats paper") {
+//     playerPoints = playerPoints + 1;
+//     playerpoints.textContent = playerPoints;
+//   } else if (computerSelection == "scissors") {
+//     playerpoints.textContent;
+//     computerpoints.textContent;
+//   } else {
+//     computerPoints = computerPoints + 1;
+//     computerpoints.textContent = computerPoints;
+//   }
+// });
